76c50b9077cab972d3f6705e6c424a1fb8788df0
tonyjhuang <huangtonyj91@gmail.com>
2020-01-22 11:12:16 -0800

    Add setActiveEvent

--- src/components/FileSelectionList.js
+++ src/components/FileSelectionList.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-export default function FileSelectionList ({events}) {
-  
+export default function FileSelectionList ({events, setActiveEvent}) {
+
   const eventsList = Object.keys(events).map((event) => {
     return (
-      <li>
+      <li 
+        key={event}
+        onClick={() => setActiveEvent(event)}
+      >
         {event}
       </li>
     )
   });
 
   return (
-    <ul>
+    <ul id="file-selection-list">
       {eventsList}
     </ul>
   );
