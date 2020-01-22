9bea7e5d619c7aaad04fe20cbf8d2f276a67b0db
tonyjhuang <huangtonyj91@gmail.com>
2020-01-21 22:06:07 -0800

    Change fileSystemApi response from array to object

--- server/fileSystemAPI.js
+++ server/fileSystemAPI.js
@@ -1,18 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-// BFS search the directory
-function getFiles(dir, fileList = []) {
+function getFiles(dir, fileList = {}) {
   fs.readdirSync(dir).forEach(file => {
     if (file === '.DS_Store') { return; }
 
-    fileList = fs.statSync(path.join(dir, file)).isDirectory()
-      ? getFiles(path.join(dir, file), fileList)
-      : fileList.concat({
-        path: path.join(dir, file),
-        file,
-        event: file.slice(0, 19)
-      });
+    if (fs.statSync(path.join(dir, file)).isDirectory()) {
+      return getFiles(path.join(dir, file), fileList)
+    } else {
+      const event = file.slice(0, 19);
+      const cam = file.slice(20, file.length - 4);
+      
+      if (!fileList[event]) {
+        fileList[event] = {}
+      }
+
+      fileList[event][cam] = path.join(dir, file);
+    }
   });
 
   return fileList;
