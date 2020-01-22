8e0f7d8b739de5cc9162e5be49b5aec31f5904d2
tonyjhuang <huangtonyj91@gmail.com>
2020-01-22 11:12:36 -0800

    Code <App/>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
-import FileNavBar from './components/FileNavBar';
+import FileSelectionList from './components/FileSelectionList';
+import Video from './components/Video';
 
-function App() {
+export default function App() {
+
+  const [events, setEvents] = useState({});
+
+  useEffect((dir) => {
+    dir = '/Users/tonyjhuang/tesla_cam_player/TeslaCam';
+
+    async function fetchEvents() {
+      const result = await axios(`http://localhost:3001/events?dir=${dir}`)
+      setEvents(result.data);
+    }
+
+    fetchEvents();
+  }, []);
+
+  const [activeEvent, setActiveEvent] = useState(); 
+
   return (
-    <div className="App">
-      <div>
-        HELLO
+    <div id="app">
+
+      <FileSelectionList
+        events={events}
+        setActiveEvent={setActiveEvent}
+      />
+
+      <div id="video-container">
+        <Video
+          angle="front"
+          filePath={events[activeEvent]}
+        />
+
+        <div id="side-angle-video-container">
+          <Video
+            angle="left_repeater"
+            filePath={events[activeEvent]}
+          />
+
+          <Video
+            angle="right_repeater"
+            filePath={events[activeEvent]}
+          />
+        </div>
       </div>
-      <FileNavBar/>
+
     </div>
   );
 }
-
-export default App;
