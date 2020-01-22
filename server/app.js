b25deef9567bfda88440f3af30c7b9ad4097fe28
tonyjhuang <huangtonyj91@gmail.com>
2020-01-22 11:11:38 -0800

    comment out cors

--- server/app.js
+++ server/app.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 const port = 3001;
 const getFiles = require('./fileSystemAPI');
 
 // cors whitelist
-var whitelist = ["http://localhost:3000"];
-var corsOptions = {
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-};
+// var whitelist = ["http://localhost:3000"];
+// var corsOptions = {
+//   origin: function(origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   }
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.get('/events', (req, res) => {
   // http://localhost:3001/events?dir=/Users/tonyjhuang/tesla_cam_player/TeslaCam
