c9ad60145645a6407faf37dbb1781dbdce8b7a61
tonyjhuang <huangtonyj91@gmail.com>
2020-01-26 10:23:14 -0800

    Draft media control inputs

--- src/App.js
+++ src/App.js
@@ -21,6 +21,51 @@ export default function App() {
 
   const [activeEvent, setActiveEvent] = useState(); 
 
+  const [mediaControl, setMediaControl] = useState({
+    play: true,
+    playbackRate: 1,
+  });
+
+  useEffect(() => {
+
+    const onKeyDown = ({key}) => {
+      const videoFront = document.getElementById('video-front');
+      const videoLeftRepeater = document.getElementById('video-left_repeater');
+      const videoRightRepeater = document.getElementById('video-right_repeater');
+      const videos = [videoFront, videoLeftRepeater, videoRightRepeater];
+
+      switch (key) {
+        case " ":
+          if (mediaControl.play) {
+            videos.forEach((video) => video.pause());
+          } else {
+            videos.forEach((video) => video.play());
+          }
+          setMediaControl({...mediaControl, play: !mediaControl.play})
+          break;
+
+        case ">":
+          const playbackRate = Math.min(mediaControl.playbackRate * 2, 16);
+          videos.forEach((video) => video.playbackRate = playbackRate);
+          setMediaControl({...mediaControl, playbackRate})
+          break;
+
+        default:
+          // console.log(key);
+          return;
+      }
+    }
+
+    document.addEventListener('keydown', onKeyDown)
+
+
+    console.log(mediaControl);
+
+    return () => {
+      document.removeEventListener('keydown', onKeyDown)
+    }
+  }, [mediaControl])
+
   return (
     <div id="app">
 
