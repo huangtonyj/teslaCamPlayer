b6d8568dba9a3e19ea1e382ccc6d4f22853a77b3
tonyjhuang <huangtonyj91@gmail.com>
2020-01-30 01:04:04 -0800

    Add cors

--- server/app.js
+++ server/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const port = 3001;
 const getFiles = require('./fileSystemAPI');
+
+// cors whitelist
+var whitelist = ["http://localhost:3000"];
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || origin === undefined) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 
 app.get('/events', (req, res) => {
   const events = getFiles(req.query.dir);
